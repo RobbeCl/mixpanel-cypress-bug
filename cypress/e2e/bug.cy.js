0c428e7403efcbeff53a36cff59d36a895a46ef2
Robbe <robbe@dexxter.be>
2022-08-16 17:16:16 +0200

    change localhost port

--- cypress/e2e/bug.cy.js
+++ cypress/e2e/bug.cy.js
@@ -2,18 +2,10 @@ import dayjs from "dayjs";
 
 describe("empty spec", () => {
   it("passes", () => {
-    const index = 0;
-    let currentMonth = 3 * (index + 1);
-    const year = dayjs().year();
+    cy.clock(dayjs(`2022-08-01`, "YYYY-MM-DD").add(5, "hour").toDate(), [
+      "Date",
+    ]);
 
-    cy.clock(
-      dayjs(`${year}-${currentMonth}-01`, "YYYY-MM-DD").add(5, "hour").toDate(),
-      ["Date"],
-      {
-        log: true,
-      }
-    );
-
-    cy.visit("http://localhost:8081");
+    cy.visit("http://localhost:8080");
   });
 });
